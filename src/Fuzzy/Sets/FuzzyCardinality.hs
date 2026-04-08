b1347913c1d4a45ebc9c2c877d5fa32f4ca1c1cc
Lukas Balog <lukasbalog66@gmail.com>
2026-04-08 19:33:44 +0200

    fixed ralescu bug

--- src/Fuzzy/Sets/FuzzyCardinality.hs
+++ src/Fuzzy/Sets/FuzzyCardinality.hs
@@ -1,5 +1,5 @@
 module Fuzzy.Sets.FuzzyCardinality(
-    fgCount, 
+    fgCount,
     feCount,
     flCount,
     ralescuF,
@@ -9,19 +9,19 @@ module Fuzzy.Sets.FuzzyCardinality(
 import FuzzySet
 import Lattices.ResiduatedLattice
 import Fuzzy.Sets.LSet (toList)
-import Data.List (sort)
+import Data.List (sort, sortBy)
 
 
 bracket :: (FuzzySet set a l) => Int -> set -> l
-bracket k set 
-    | null alphas = 0 
+bracket k set
+    | null alphas = 0
     | otherwise   = maximum alphas
-    where 
+    where
         alphas = [alpha | alpha <- truthDegrees set, length (alphaCut alpha set) >= k]
 
 
 fgCount :: (FuzzySet set a l, FuzzySet countSet Int l) => set -> countSet
-fgCount set = mkFuzzySet f universeCounts 
+fgCount set = mkFuzzySet f universeCounts
     where
         universeCounts = [0 .. universeCardinality set]
         f k            = if k == 0 then 1 else bracket k set
@@ -31,14 +31,15 @@ flCount :: (FuzzySet set a l, FuzzySet countSet Int l) => set -> countSet
 flCount set = mkFuzzySet f universeCounts
     where
         universeCounts = [0 .. universeCardinality set]
-        f k            = if k == 0 then 0 else negation $ bracket (k + 1) set
+        f k            = negation $ bracket (k + 1) set
+
 
 feCount :: (FuzzySet set a l, FuzzySet countSet Int l) => set -> countSet
 feCount set  = intersection (flCount set) (fgCount set)
 
 ralescuF :: (FuzzySet set a l, FuzzySet countSet Int l) => set -> countSet
 ralescuF set = mkFuzzySet f universeCounts
-    where 
+    where
         universeCounts = [0 .. universeCardinality set]
-        f k  = (degs !! k) /\ negation (degs !! k + 1)
-        degs = 1 : sort (truthDegrees set) ++ [0]
+        f k  = (degs !! k) /\ negation (degs !! (k + 1))
+        degs = 1 : sortBy (flip compare) (truthDegrees set) ++ [0]
--- test/Fuzzy/Sets/FuzzyCardinalityTest.hs
+++ test/Fuzzy/Sets/FuzzyCardinalityTest.hs
@@ -33,7 +33,7 @@ testBracket =
 testFgCount :: Assertion
 testFgCount = do
     let c = fgCount whereSet :: LSet Int UILukasiewicz
-        expected = [(0,0.7),(1,0.7),(2,0.5),(3,0.4),(4,0.2),(5,0.2),(6,0.1),(7,0.1)]
+        expected = [(0,1),(1,0.7),(2,0.5),(3,0.4),(4,0.2),(5,0.2),(6,0.1),(7,0.1)]
         actual = [(k, fromLukasiewiczUnitInterval v) | (k,v) <- toList c]
         pairs = zip expected actual
     mapM_ (\(e,a) -> do
@@ -69,7 +69,7 @@ testFeCount = do
 testRalescuF :: Assertion
 testRalescuF = do
     let c = ralescuF whereSet :: LSet Int UILukasiewicz
-        expected = [(0,0.9),(1,0.1),(2,0.1),(3,0.2),(4,0.2),(5,0.4),(6,0.3),(7,0.7)]
+        expected = [(0,0.3),(1,0.5),(2,0.5),(3,0.4),(4,0.2),(5,0.2),(6,0.1),(7,0.1)]
         actual = [(k, fromLukasiewiczUnitInterval v) | (k,v) <- toList c]
         pairs = zip expected actual
     mapM_ (\(e,a) -> do
